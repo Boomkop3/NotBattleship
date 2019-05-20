25d9b2851dd2406462d2a73e5a6d9f53f30642f1
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-05-20 13:34:50 +0200

    Removed depricated //ToDo comment

--- src/com/unplayable/Static/ImageLibrary.java
+++ src/com/unplayable/Static/ImageLibrary.java
@@ -7,7 +7,6 @@ public class ImageLibrary {
     public final BufferedImage SHIP_PIECE_DESTROYED_OVERLAY;
     private static ImageLibrary instance;
     private ImageLibrary() throws Exception {
-        // ToDo insert image from resources
         this.SHIP_PIECE_DESTROYED_OVERLAY = ImageIO.read(getClass().getResource("/Sprites/exploded.png"));
     }
 
