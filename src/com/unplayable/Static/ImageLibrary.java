154c2170d7121bdbea73563b8203d9be50c0e57c
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-05-20 13:15:46 +0200

    Added image library

--- src/com/unplayable/Static/ImageLibrary.java
+++ src/com/unplayable/Static/ImageLibrary.java
@@ -1,4 +1,20 @@
 package com.unplayable.Static;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
 public class ImageLibrary {
+    private final BufferedImage SHIP_PIECE_DESTROYED_OVERLAY;
+    private ImageLibrary instance;
+    private ImageLibrary(){
+        // ToDo insert image from resources
+        this.SHIP_PIECE_DESTROYED_OVERLAY = ImageIO.read();
+    }
+
+    public ImageLibrary getInstance(){
+        if (this.instance == null){
+            this.instance = new ImageLibrary();
+        }
+        return this.instance;
+    }
 }
