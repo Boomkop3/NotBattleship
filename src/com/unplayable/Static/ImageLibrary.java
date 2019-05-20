e1fb9b1ec2d36105286074637d3de0a771c5392a
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-05-20 13:23:44 +0200

    Made getinstance static

--- src/com/unplayable/Static/ImageLibrary.java
+++ src/com/unplayable/Static/ImageLibrary.java
@@ -5,16 +5,16 @@ import java.awt.image.BufferedImage;
 
 public class ImageLibrary {
     private final BufferedImage SHIP_PIECE_DESTROYED_OVERLAY;
-    private ImageLibrary instance;
+    private static ImageLibrary instance;
     private ImageLibrary(){
         // ToDo insert image from resources
         this.SHIP_PIECE_DESTROYED_OVERLAY = ImageIO.read();
     }
 
-    public ImageLibrary getInstance(){
-        if (this.instance == null){
-            this.instance = new ImageLibrary();
+    public static ImageLibrary getInstance(){
+        if (instance == null){
+            instance = new ImageLibrary();
         }
-        return this.instance;
+        return instance;
     }
 }
