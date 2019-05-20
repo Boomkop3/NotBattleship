0522219a4826eba3bafa7ac7dc7c707fd7d19e65
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-05-20 13:55:56 +0200

    threw error on render fail, added setposition to ship

--- src/com/unplayable/Ship/Ship.java
+++ src/com/unplayable/Ship/Ship.java
@@ -1,5 +1,7 @@
 package com.unplayable.Ship;
 
+import org.jfree.fx.FXGraphics2D;
+
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
@@ -8,17 +10,54 @@ import java.util.Arrays;
 public abstract class Ship {
     private ShipPiece[] pieces;
     private Point2D position;
+    private double rotation;
 
-    public void setPosition(Point2D position){
+    public void setPosition(Point2D position, Rotation rotation){
         this.position = position;
-        // ToDo update ship piece positions
+        ShipPiece[] shipPieces = this.pieces;
+        int verticalOffset = 0;
+        int horizontalOffset = 0;
+        switch (rotation) {
+            case North: {
+                verticalOffset = 1;
+                break;
+            }
+            case South: {
+                verticalOffset = -1;
+                break;
+            }
+            case East: {
+                horizontalOffset = 1;
+                break;
+            }
+            case West: {
+                horizontalOffset = -1;
+                break;
+            }
+        }
+        for (int i = 0, shipPiecesLength = shipPieces.length; i < shipPiecesLength; i++) {
+            ShipPiece piece = shipPieces[i];
+            int size = piece.getImage().getHeight();
+            piece.setPosition(
+                new Point2D.Double(
+                    (int)this.position.getX() + (i * horizontalOffset),
+                    (int)this.position.getY() + (i * verticalOffset)
+                )
+            );
+        }
     }
 
     public Ship(String sprite){
         //ToDo split sprite into ship pieces
     }
 
-    public void draw(){
-        Arrays.stream(this.pieces).parallel().forEach(piece -> piece.draw());
+    public void draw(FXGraphics2D g) {
+        Arrays.stream(this.pieces).parallel().forEach(piece -> {
+            try {
+                piece.draw(g);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
