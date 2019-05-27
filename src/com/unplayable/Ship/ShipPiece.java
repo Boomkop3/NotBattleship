c751080285868405a2d69849b283cbf11b95e6d7
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-05-27 11:08:25 +0200

    Removed position variable from shippiece

--- src/com/unplayable/Ship/ShipPiece.java
+++ src/com/unplayable/Ship/ShipPiece.java
@@ -7,19 +7,27 @@ import org.dyn4j.geometry.Vector2;
 import org.jfree.fx.FXGraphics2D;
 
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 public class ShipPiece extends Body {
     private boolean destroyed;
     private BufferedImage image;
-    private Point2D position;
 
     public boolean isDestroyed() {
         return this.destroyed;
     }
 
     public void draw(FXGraphics2D g) throws Exception {
+        System.out.println(this.getTransform());
+        g.draw(
+            new Ellipse2D.Double(
+                (int)this.getTransform().getTranslationX(), this.getTransform().getTranslationY(), 25, 25
+            )
+        );
+
+        /*
         AffineTransform at = new AffineTransform();
         g.drawImage(this.image, (int)this.position.getX(), (int)this.position.getY(), null);
         // Draw base layer
@@ -28,10 +36,11 @@ public class ShipPiece extends Body {
                 ImageLibrary.getInstance().SHIP_PIECE_DESTROYED_OVERLAY,
                 (int)this.position.getX(), (int)this.position.getY(), null);
         }
+        */
     }
 
     public void setPosition(Point2D position) {
-        this.position = position;
+
     }
 
     public ShipPiece(BufferedImage image){
