9f8cdb3357d456c08360529ccc7fb83459938707
Sjors de Bruijn <sjors.de.bruijn@gmail.com>
2019-05-27 22:27:01 +0200

    Implemented the updated Rotation enum

--- src/com/unplayable/Ship/Ship.java
+++ src/com/unplayable/Ship/Ship.java
@@ -1,11 +1,14 @@
 package com.unplayable.Ship;
 
+import com.unplayable.Static.GlobalVariables;
+import com.unplayable.Static.ImageLibrary;
 import org.dyn4j.dynamics.World;
 import org.dyn4j.dynamics.joint.WeldJoint;
 import org.dyn4j.geometry.Vector2;
 import org.jfree.fx.FXGraphics2D;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
@@ -13,40 +16,20 @@ import java.util.Arrays;
 public class Ship {
     private ShipPiece[] pieces;
     private double rotation;
+    private BufferedImage sprite;
 
     public void setPosition(int x, int y, Rotation rotation){
     	this.setPosition(new Point2D.Double(x, y), rotation);
 	}
 
     public void setPosition(Point2D position, Rotation rotation){
-        int verticalOffset = 0;
-        int horizontalOffset = 0;
-        switch (rotation) {
-            case North: {
-                verticalOffset = 1;
-                break;
-            }
-            case South: {
-                verticalOffset = -1;
-                break;
-            }
-            case East: {
-                horizontalOffset = 1;
-                break;
-            }
-            case West: {
-                horizontalOffset = -1;
-                break;
-            }
-        }
-
         for (int i = 0, shipPiecesLength = this.pieces.length; i < shipPiecesLength; i++) {
             ShipPiece piece = this.pieces[i];
             int size = piece.getImage().getHeight();
             piece.setPosition(
                 new Point2D.Double(
-                    (int)position.getX() + (i * horizontalOffset * size),
-                    (int)position.getY() + (i * verticalOffset * size)
+                    (int)position.getX() + (i * rotation.offset.getX() * size),
+                    (int)position.getY() + (i * rotation.offset.getY() * size)
                 )
             );
         }
@@ -54,6 +37,7 @@ public class Ship {
 
     public Ship(BufferedImage sprite, World world) {
         // Ship sprites are stored horizontally
+		this.sprite = sprite;
         int size = sprite.getWidth();
         this.pieces = new ShipPiece[sprite.getHeight()/size];
         for (int i = 0; i < pieces.length; i++) {
