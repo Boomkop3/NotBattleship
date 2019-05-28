254ff10d40b651efcbdb615010e0b8faa6dc5710
Sjors de Bruijn <sjors.de.bruijn@gmail.com>
2019-05-28 16:59:34 +0200

    Removed redundant code, added test code

--- src/com/unplayable/Main.java
+++ src/com/unplayable/Main.java
@@ -1,10 +1,8 @@
 package com.unplayable;
 
-import com.unplayable.Gui.SeaWorld;
-import com.unplayable.Static.ImageLibrary;
+import com.unplayable.Networking.Connection;
+import com.unplayable.Networking.ConnectionManager;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
@@ -13,20 +11,72 @@ public class Main extends Application {
     public static void main(String[] args) throws Exception{
         launch(Main.class);
     }
-
+	int eventsRecieved = 0;
     @Override
     public void start(Stage stage) throws Exception {
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(
-            new SeaWorld((g)->draw(g), borderPane)
+        //BorderPane borderPane = new BorderPane();
+
+		ConnectionManager.getInstance();
+
+		new Thread(()->{
+			try {
+
+
+				ConnectionManager.getInstance().receiveObject(
+					(result, adress)->{
+						System.out.println((String)result);
+						System.out.println("from: " + adress);
+					}
+				);
+
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+		new Thread(()->{
+			try {
+
+
+				while (true){
+					ConnectionManager connectionManager = ConnectionManager.getInstance();
+					Connection connection = connectionManager.createConnection("127.0.0.1");
+					connection.sendObject("get fkn rekt m8");
+				}
+
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+		/*
+        borderPane.setLeft(
+			new TextArea()
         );
+		borderPane.setCenter(
+			new TextField()
+		);
+		borderPane.setRight(
+			new Button("send message")
+		);
+
         stage.setScene(
             new Scene(
                 borderPane
             )
         );
         stage.show();
+        */
     }
+
+    private void inputReceived(Object object){
+    	eventsRecieved++;
+    	String wtvr = (String)object;
+    	System.out.println(wtvr);
+    	System.out.println("recieved " + eventsRecieved);
+	}
 
     public void draw(FXGraphics2D g){
 
