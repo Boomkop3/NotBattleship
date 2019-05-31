29d65ac05daa1c85c09a9427beeb10779d8a7b41
Sjors de Bruijn <sjors.de.bruijn@gmail.com>
2019-05-31 16:39:51 +0200

    Enabled global debugging variable

--- src/com/unplayable/Static/GlobalVariables.java
+++ src/com/unplayable/Static/GlobalVariables.java
@@ -3,5 +3,5 @@ package com.unplayable.Static;
 public class GlobalVariables {
     public static final int shipPieceSize = 35;
     public static final int boardWidthHeight = 12;
-    public static boolean debug = false;
+    public static boolean debug = true;
 }
