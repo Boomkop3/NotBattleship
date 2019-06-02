7e253bc5ad22074060638108c45cdc261d4b8545
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-02 15:20:44 +0200

    Created ConnectionWindow Class

--- src/com/unplayable/Gui/ConnectionWindow.java
+++ src/com/unplayable/Gui/ConnectionWindow.java
@@ -1,5 +1,21 @@
 package com.unplayable.Gui;
 
-public class ConnectionWindow {
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 
+public class ConnectionWindow extends BorderPane {
+    public ConnectionWindow(){
+        this.setLeft(
+            new Label("Enter server ip: ")
+        );
+        this.setCenter(
+            new TextField()
+        );
+        this.setRight(
+            new Button("Connect")
+        );
+    }
 }
