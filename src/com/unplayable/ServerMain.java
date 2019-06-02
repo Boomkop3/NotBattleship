dd10de28d6d07dc458eccb7ec49f62e72f64d984
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-02 14:46:53 +0200

    Implemented connection counter

--- src/com/unplayable/ServerMain.java
+++ src/com/unplayable/ServerMain.java
@@ -1,12 +1,22 @@
 package com.unplayable;
 
 import com.unplayable.Gui.ServerWindow;
+import com.unplayable.Networking.Connection;
+import com.unplayable.Networking.ConnectionManager;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.List;
+
 public class ServerMain {
     public static void main(String[] args) {
-        
+        System.out.println("Running server...");
+        ConnectionManager manager = ConnectionManager.getInstance();
+        manager.getInstance().setOnConnection(
+            ()->{
+                System.out.println("clients connected: " + manager.getIncomingConnections().size());
+            }
+        );
     }
 }
