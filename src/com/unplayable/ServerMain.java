dbcc9411c5cee68f8e9ee0f1df1305c05643e4d3
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-02 15:19:47 +0200

    Added auto game lobby

--- src/com/unplayable/ServerMain.java
+++ src/com/unplayable/ServerMain.java
@@ -7,16 +7,33 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServerMain {
+    private static List<Connection> connectedPlayers;
     public static void main(String[] args) {
+        connectedPlayers = new ArrayList<>();
         System.out.println("Running server...");
         ConnectionManager manager = ConnectionManager.getInstance();
         manager.getInstance().setOnConnection(
             ()->{
-                System.out.println("clients connected: " + manager.getIncomingConnections().size());
+                List<Connection> connections = manager.getIncomingConnections();
+                int connectionCount = connections.size();
+                System.out.println("clients connected: " + connectionCount);
+                if (connectionCount > 0 && connectionCount % 2 == 0){
+                    startGame(
+                        connections.get(connectionCount-2),
+                        connections.get(connectionCount-1)
+                    );
+                }
             }
         );
+    }
+
+    private static void startGame(Connection player1, Connection player2) {
+        System.out.println("starting game between: ");
+        System.out.println("> " + player1.getIp());
+        System.out.println("> " + player2.getIp());
     }
 }
