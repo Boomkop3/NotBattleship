d50745efba8ad09e8485b6afeea5ed867ddbd0bb
Tom <tomkaas74@gmail.com>
2019-06-02 20:02:02 +0200

    Made variables private

--- src/com/unplayable/Gui/Score.java
+++ src/com/unplayable/Gui/Score.java
@@ -1,14 +1,14 @@
 package com.unplayable.Gui;
 
 public class Score {
-    int hits;
-    int hitsRecieved;
-    int kills;
-    int deaths;
-    int ownHitsToGo;
-    int otherHitsToGo;
-    int ownKillsToGo;
-    int otherKillsToGo;
+    private int hits;
+    private int hitsRecieved;
+    private int kills;
+    private int deaths;
+    private int ownHitsToGo;
+    private int otherHitsToGo;
+    private int ownKillsToGo;
+    private int otherKillsToGo;
 
     public Score(int hits, int hitsRecieved, int kills, int deaths, int ownHitsToGo, int otherHitsToGo, int ownKillsToGo, int otherKillsToGo) {
         this.hits = hits;
