6f3e169af15567a21261368621870d95fcdca027
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-02 16:55:33 +0200

    Added global start game command

--- src/com/unplayable/Static/GlobalVariables.java
+++ src/com/unplayable/Static/GlobalVariables.java
@@ -6,4 +6,5 @@ public class GlobalVariables {
     public static boolean debug = true;
     public static final int explosionDistance = 60;
     public static final int explosionForce = 100000;
+    public static final String startGameCommand = "startgameplz";
 }
