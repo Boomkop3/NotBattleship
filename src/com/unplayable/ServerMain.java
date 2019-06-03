81c38a21e303b36167610afc25b7cea7b60e588c
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-03 13:00:58 +0200

    Uhm, I didn't forget this... Trust me...

--- src/com/unplayable/ServerMain.java
+++ src/com/unplayable/ServerMain.java
@@ -25,6 +25,8 @@ public class ServerMain {
                         connections.get(connectionCount-2),
                         connections.get(connectionCount-1)
                     );
+                    connections.remove(connectionCount-1);
+                    connections.remove(connectionCount-1);
                 }
             }
         );
