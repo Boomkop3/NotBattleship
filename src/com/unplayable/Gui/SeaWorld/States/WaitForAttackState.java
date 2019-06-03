7d55790bd363dc13bfb3de7c33aff2c2ac9dd9ca
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-03 12:37:27 +0200

    Added statistics update

--- src/com/unplayable/Gui/SeaWorld/States/WaitForAttackState.java
+++ src/com/unplayable/Gui/SeaWorld/States/WaitForAttackState.java
@@ -1,6 +1,8 @@
 package com.unplayable.Gui.SeaWorld.States;
 
+import com.unplayable.Gui.GameStateView;
 import com.unplayable.Gui.PlayWindow;
+import com.unplayable.Gui.Score;
 import com.unplayable.Gui.SeaWorld.SeaWorld;
 import javafx.scene.input.MouseEvent;
 
@@ -35,12 +37,33 @@ public class WaitForAttackState extends SeaWorldState {
 	public void handleInput(String message) {
 		System.out.println("Recieved message: ");
 		System.out.println(message);
+
+		if (message.startsWith("igothit:")){
+			System.out.println("recieved enemy hit info");
+			String[] messages = message.split(":");
+			int enemyDestroyedPieceCount = Integer.valueOf(messages[1]);
+			int enemyDeathCount = Integer.valueOf(messages[2]);
+			GameStateView gameStateView = ((GameStateView)((PlayWindow)this.seaWorld.getParent()).getLeft());
+			Score newScore = gameStateView.getScore();
+			newScore.setKills(enemyDeathCount);
+			newScore.setHits(enemyDestroyedPieceCount);
+			newScore.setHitsRecieved(this.seaWorld.getDestroyedPieceCount());
+			newScore.setDeaths(this.seaWorld.getKillCount());
+			newScore.setOwnKillsToGo(this.seaWorld.getShipCount() - enemyDeathCount);
+			newScore.setOwnHitsToGo(this.seaWorld.getPieceCount() - enemyDestroyedPieceCount);
+			newScore.setOtherHitsToGo(this.seaWorld.getPieceCount() - this.seaWorld.getDestroyedPieceCount());
+			newScore.setOtherKillsToGo(this.seaWorld.getShipCount() - this.seaWorld.getKillCount());
+			gameStateView.setScore(newScore);
+			gameStateView.updateInterface();
+		}
+
 		if (message.startsWith("hit:")){
 			String[] coords = message.split(":");
 			int x = Integer.valueOf(coords[1]);
 			int y = Integer.valueOf(coords[2]);
 			this.seaWorld.createExplosion(x, y);
 			try {
+				System.out.println("Sending I got hit");
 				((PlayWindow)this.seaWorld.getParent()).getConnection().writeString(
 					"igothit:"
 						+ this.seaWorld.getDestroyedPieceCount()
