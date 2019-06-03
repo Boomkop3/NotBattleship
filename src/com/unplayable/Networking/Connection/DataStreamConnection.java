9e94b3d79507afeee3b4adc5b363c441fae28584
Sjors de Bruijn <sjors.de.bruijn@gmail.com>
2019-06-03 04:42:33 +0200

    Implemented datastream connection

--- src/com/unplayable/Networking/Connection/DataStreamConnection.java
+++ src/com/unplayable/Networking/Connection/DataStreamConnection.java
@@ -4,11 +4,29 @@ import com.unplayable.Gui.Score;
 import com.unplayable.Networking.ScoreNetworkCallback;
 import com.unplayable.Networking.StringNetworkCallback;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class DataStreamConnection implements ConnectionBackEnd {
-
 	private Socket socket;
+	private DataInputStream dataInputStream;
+	private DataOutputStream dataOutputStream;
+
+	private DataOutputStream getDataOutputStream() throws IOException {
+		if (this.dataOutputStream == null){
+			this.dataOutputStream = new DataOutputStream(this.socket.getOutputStream());
+		}
+		return this.dataOutputStream;
+	}
+
+	private DataInputStream getDataInputStream() throws IOException {
+		if (this.dataInputStream == null){
+			this.dataInputStream = new DataInputStream(this.socket.getInputStream());
+		}
+		return this.dataInputStream;
+	}
 
 	public DataStreamConnection(Socket socket){
 		this.socket = socket;
@@ -21,11 +39,22 @@ public class DataStreamConnection implements ConnectionBackEnd {
 
 	@Override
 	public void writeString(String string) {
-
+		try {
+			DataOutputStream outputStream = this.getDataOutputStream();
+			outputStream.writeUTF(string);
+			outputStream.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public String readString() {
+		try {
+			return getDataInputStream().readUTF();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
@@ -36,12 +65,20 @@ public class DataStreamConnection implements ConnectionBackEnd {
 
 	@Override
 	public void readStringAsync(StringNetworkCallback callback) {
-
+		new Thread(()->{
+			callback.callback(
+				this.readString()
+			);
+		}).start();
 	}
 
 	@Override
 	public void readScoreAsync(ScoreNetworkCallback callback) {
-
+		new Thread(()->{
+			callback.callback(
+				this.readScore()
+			);
+		}).start();
 	}
 
 	@Override
